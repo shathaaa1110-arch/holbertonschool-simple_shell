6f0e10276138c860a3b383730209e151b214446b
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-15 22:58:57 +0000

    Task 2

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "shell.h"
 
 /**
- * main - Entry point for the simple shell program
+ * main - Entry point for the simple shell
  * @argc: The number of arguments passed to the program
  * @argv: An array of strings containing the arguments
  *
@@ -14,21 +11,60 @@ int main(int argc, char *argv[])
 {
 	char *line = NULL;
 	size_t len = 0;
-	int count = 0;
+	ssize_t nread;
+	pid_t child_pid;
+	int status;
+	char *args[2];
 	(void)argc;
 
-	while (getline(&line, &len, stdin) != -1)
+	while (1)
 	{
-		count++;
-		line[strcspn(line, "\n")] = '\0';
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-		if (access(line, X_OK) == -1)
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
 		{
-			fprintf(stderr, "%s: %d: %s: not found\n",
-				argv[0], count, line);
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		if (nread > 0 && line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+
+		if (strlen(line) == 0)
+			continue;
+
+		args[0] = line;
+		args[1] = NULL;
+
+		if (access(line, X_OK) == 0)
+		{
+			child_pid = fork();
+			if (child_pid == -1)
+			{
+				perror("Error:");
+				break;
+			}
+			if (child_pid == 0)
+			{
+				if (execve(args[0], args, environ) == -1)
+				{
+					perror(argv[0]);
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				wait(&status);
+			}
+		}
+		else
+		{
+			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
 		}
 	}
-
 	free(line);
 	return (0);
 }
