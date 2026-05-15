ec39bd83de82f5d92b71b16da21b4854c65e5261
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-15 20:49:25 +0000

    Task 1

--- main.c
+++ main.c
@@ -11,23 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
-    char *line = NULL;
-    size_t len = 0;
-    int count = 0;
+char *line = NULL;
+size_t len = 0;
+int count = 0;
 
-    while (getline(&line, &len, stdin) != -1)
-    {
-        count++;
-
-        line[strcspn(line, "\n")] = '\0';
-
-        if (access(line, X_OK) == -1)
-        {
-            fprintf(stderr, "%s: %d: %s: not found\n",
-                    argv[0], count, line);
-        }
-    }
-
-    free(line);
-    return (0);
+while (getline(&line, &len, stdin) != -1)
+{
+count++;
+line[strcspn(line, "\n")] = '\0';
+if (access(line, X_OK) == -1)
+{
+fprintf(stderr, "%s: %d: %s: not found\n",argv[0], count, line);
+}
+}
+free(line);
+return (0);
 }
