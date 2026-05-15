aeed37a4e39a653f28854cdb08e8a8837a8b6a39
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-15 20:45:11 +0000

    Task 1

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/*
+ *main - Entry point for the hsh simple shell.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+    char *line = NULL;
+    size_t len = 0;
+    int count = 0;
+
+    while (getline(&line, &len, stdin) != -1)
+    {
+        count++;
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if (access(line, X_OK) == -1)
+        {
+            fprintf(stderr, "%s: %d: %s: not found\n",
+                    argv[0], count, line);
+        }
+    }
+
+    free(line);
+    return (0);
+}
---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/**
+ * file: shell.h
+ * desc: Header file containing declarations for all functions
+ *       used in the holbertonschool-simple_shell project.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+int main(void);
+
+#endif /* SHELL_H */
